86289d4012061e721ede5f3af65ef6754cb2457a
unknown <yotuba.story@gmail.com>
2026-05-08 18:49:37 +0900

    PWA対応

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'rouduku-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './favicon.ico',
+    './apple-touch-icon.png',
+    './android-chrome-192x192.png',
+    './android-chrome-512x512.png'
+];
+
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
